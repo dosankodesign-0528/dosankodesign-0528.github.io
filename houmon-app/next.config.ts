b2ffcf588ebae4ebe9f6ab0b7b992335cedbf9fc
Yamanaka Hideyuki <hideyuki@Mac.lan>
2026-04-18 16:25:11 +0900

    Remove houmon-app (extracting to separate repo)

--- houmon-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  basePath: process.env.BASE_PATH || '',
-  output: 'export',
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
